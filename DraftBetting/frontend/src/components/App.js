bda10b91105676dc502e6f8a0d092d4f61aca1de
Jacob <simerly81@gmail.com>
2022-02-05 16:36:28 -0500

    pre custom fixes

--- DraftBetting/frontend/src/components/App.js
+++ DraftBetting/frontend/src/components/App.js
@@ -9,9 +9,9 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
+            <html>
                 <HomePage />
-            </div>
+            </html>
         );
     }
 }
