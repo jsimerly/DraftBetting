0b667cd0d46fa2df49cd3b58d0811e388e938181
Jacob <simerly81@gmail.com>
2022-02-14 18:43:06 -0500

    started on some front end

--- DraftBetting/frontend/src/components/RegisterPage.js
+++ DraftBetting/frontend/src/components/RegisterPage.js
@@ -1,4 +1,22 @@
 import React, { Component } from "react";
+import { 
+    Button, 
+    Grid, 
+    Typography, 
+    FormControl,
+    Form,
+    Input,
+    InputLabel,
+    MenuItem,
+    Divider, 
+    Select,
+    Menu,
+    Stack,
+    Item,
+    List,
+    ListItem,
+    ListItemText
+ } from '@material-ui/core';
 
 export default class RegisterPage extends Component {
     constructor(props){
@@ -12,6 +30,40 @@ export default class RegisterPage extends Component {
 
 
     render(){
-        return <h1>Register Page</h1>
+        return (
+            <div>
+                <FormControl>
+                    <InputLabel> e-mail </InputLabel>
+                    <Input
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+
+                    />
+                </FormControl>
+                <FormControl>
+                    <InputLabel> password </InputLabel>
+                        <Input
+                            name="password"
+                            type="password"
+                            autoComplete="password"
+                        />
+                </FormControl>
+                <FormControl>
+                    <InputLabel> confirm password </InputLabel>
+                        <Input
+                            name="password"
+                            type="password"
+                            autoComplete="password"
+                        />
+                </FormControl>
+
+                <Button
+                    variant="contained" color="primary"
+                >
+                    Register
+                </Button>
+            </div>
+        )
     }
 }
